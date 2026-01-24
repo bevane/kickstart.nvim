e608aec84b5541a8009520840e65db4991a9540e
Bevane <bevane@live.com>
2026-01-24 17:07:47 +0700

    feat: add tailwindcss lsp

--- init.lua
+++ init.lua
@@ -703,6 +703,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
+        tailwindcss = {},
         astro = {},
         jq = {},
         jsonls = {
